238ca4ddc35a5a0db698335e611b00e0b173dfb7
Sushmitha Jain <sushmitha.jain@iihs.in>
2024-07-22 14:36:55 +0530

    added

---
+++ books/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import BookListCreateView, BookRetrieveUpdateDestroyView
+
+urlpatterns = [
+    path('books/', BookListCreateView.as_view(), name='book-list-create'),
+    path('books/<int:pk>/', BookRetrieveUpdateDestroyView.as_view(), name='book-detail'),
+]
---
+++ books/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+
+class BookListCreateView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+def home_view(request):
+    return HttpResponse("Welcome to the Book Management API. Visit /api/books/ to interact with the API.")
+
